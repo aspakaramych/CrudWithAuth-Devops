4ba256e59091f872fed9a04138aa69373741e2f3
aspakaramych <vgrig2006@gmail.com>
2026-02-12 11:52:22 +0700

    c

---
+++ CrudWithAuth/DTOs/RegisterRequest.cs
@@ -0,0 +1,8 @@
+namespace CrudWithAuth.DTOs;
+
+public class RegisterRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
